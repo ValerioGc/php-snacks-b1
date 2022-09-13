ffad44d84d32dd4b47906381fb99739555778853
ValerioGc <valerio.gc1@gmail.com>
2022-09-13 09:41:32 +0200

    Correzione snack 2

--- Snack-2/index.php
+++ Snack-2/index.php
@@ -4,6 +4,9 @@
     $name = trim($_GET['name']);
     $age = trim($_GET['age']);
     $email = trim($_GET['email']);
+    $status = 'error';
+    $status2 = 'error';
+    $status3 = 'error';
     $accountInfo = [];
 
 
@@ -12,24 +15,29 @@
         if (is_string($name) && (strlen($name) > 3)) {
             $accountInfo['name'] = $name;
             var_dump($accountInfo['name']);
+            $status = 'ok';
         }
         elseif (!is_string($name) || (strlen($name) <= 3)) {
-            $accountInfo['name'] = 'formato nome sbagliato';
+            $accountInfo['name'] = 'errore';
         }
 
         if (is_numeric($age) && (strlen($age) != 0)) {
             $accountInfo['age'] = $age;
+            $status2 = 'ok';
+
         }
         elseif (!is_numeric($age) || (strlen($age) == 0)) {
-            $accountInfo['age'] = 'formato età sbagliato';
+            $accountInfo['age'] = 'errore';
         }
 
         if (is_string($email) && (strlen($email) != 0)) {
                if ( (strpos($email, '@')) && (strpos($email, '.')) ) {
                     $accountInfo['email'] = $email;
+                    $status3 = 'ok';
+
                }
                else {
-                   $accountInfo['email'] = 'formato email sbagliato';
+                   $accountInfo['email'] = 'errore';
                }
         }
 
@@ -42,10 +50,10 @@
         echo "<br/>";
         echo "<br/>";
 
-        if (count($accountInfo) < 3) {
-            echo "<h1>Inserisci correttamente i dati</h1>";
+        if ((count($accountInfo) == 3)  && ($status == 'ok') && ($status2 == 'ok') && ($status3 == 'ok')){
+            echo "<h1>Accesso Riuscito</h1>";
         }
         else {
-            echo "<h1>Accesso Riuscito</h1>";
+            echo "<h1>Inserisci correttamente i dati</h1>";
         };
 
